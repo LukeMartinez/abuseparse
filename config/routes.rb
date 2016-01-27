ce3d6aeb943bf712be055ddeace2e59642c93fe5
Luke Martinez <luke.martinez@sendgrid.com>
2016-01-27 13:56:39 -0700

    email processor

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Parseapi::Application.routes.draw do
 
 
   resources :posts
+
+  get "/email_processor"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
