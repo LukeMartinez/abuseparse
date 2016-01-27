781f09000aafebe28d634a49ad6fbdc775b380fd
Luke Martinez <luke.martinez@sendgrid.com>
2016-01-27 13:35:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parseapi::Application.config.secret_key_base = '998b0ade16896b33db2b2b6d40ae1d0a0274e61edc50687e2b85b0a2e51f297469e7612554ee1299c3a1dc82ed9f339dd9920f1fe66078e77079b63ed5633611'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Parseapi::Application.config.session_store :cookie_store, key: '_parseapi_session'
